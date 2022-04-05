7e2ba83d815f6f5bfc8b448a32ba8fbea717342e
Caio Imbroisi <caio.imbroisi@gmail.com>
2022-04-05 05:40:18 -0300

    exercicios blc 10 dia 1

---
+++ front-end/b10-introducao-react/exercicios-do-dia/exercicios/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (<footer> Footer da Página, NiNgUéM CoRp. LTDA - #VQV</footer>)
+    }
+}
+
+export default Footer;
